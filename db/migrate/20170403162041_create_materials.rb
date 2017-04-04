050c2aec211f6970d9a34b00b8c4c5426672b529
Michael Pet <mixty01@gmail.com>
2017-04-04 20:40:54 +0600

    Change references to belongs_to in materials migration

--- db/migrate/20170403162041_create_materials.rb
+++ db/migrate/20170403162041_create_materials.rb
@@ -1,15 +1,15 @@
 class CreateMaterials < ActiveRecord::Migration[5.0]
   def change
     create_table :materials do |t|
-      t.references :rightholder, index: true, foreign_key: { to_table: :rightholders }
+      t.belongs_to :rightholder, index: true
       t.references :owner, index: true, foreign_key: { to_table: :users }
       t.string :original_link, null: true
       t.string :caption_original, null: false
       t.string :caption_translated, null: true
       t.text :annotation_original, null: false
       t.text :annotation_translated, null: true
-      t.references :state, index: true, foreign_key: { to_table: :states }
-      t.references :license, index: true, foreign_key: { to_table: :licenses }
+      t.belongs_to :state, index: true
+      t.belongs_to :license, index: true
       t.string :original_language, null: false
       t.string :translation_language, null: false
       t.integer :tags, array: true, null: false
