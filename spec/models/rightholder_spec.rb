638bc70977900c63dc64f4b99321f2c80828adf7
Дмитрий Басавин <basavind@gmail.com>
2017-04-09 11:54:06 +0300

    ✅  add validation tests for rightholder model

---
+++ spec/models/rightholder_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Rightholder, type: :model do
+  # Validations
+  it { should validate_presence_of(:caption_original) }
+  it { should validate_presence_of(:short_original) }
+
+  # Associations
+  it { should have_many(:materials) }
+end
