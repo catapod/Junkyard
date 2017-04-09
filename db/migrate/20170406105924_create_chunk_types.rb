8308026af78a4526e3f2bde7b9fac77b127769bc
Дмитрий Басавин <basavind@gmail.com>
2017-04-09 12:29:42 +0300

    💥  rename slice types migration to chunk types

--- db/migrate/20170406105924_create_chunk_types.rb
+++ db/migrate/20170406105924_create_chunk_types.rb
@@ -1,6 +1,6 @@
-class CreateSliceTypes < ActiveRecord::Migration[5.0]
+class CreateChunkTypes < ActiveRecord::Migration[5.0]
   def change
-    create_table :slice_types do |t|
+    create_table :chunk_types do |t|
       t.string :name, null: false
       t.string :display_name, null: false
     end
