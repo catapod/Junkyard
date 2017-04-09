bc85cafdeff43bda7571ef939133c84aebb80324
Дмитрий Басавин <basavind@gmail.com>
2017-04-09 10:56:38 +0300

    ✅  validation tests for material model

---
+++ spec/models/material_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Material, type: :model do
+  # Validations
+  it { should validate_presence_of(:owner) }
+  it { should validate_presence_of(:rightholder) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:license) }
+  it { should validate_presence_of(:caption_original) }
+  it { should validate_presence_of(:annotation_original) }
+  it { should validate_presence_of(:original_language) }
+  it { should validate_presence_of(:translation_language) }
+
+  # Associations
+  it { should belong_to(:owner) }
+  it { should belong_to(:rightholder) }
+  it { should belong_to(:state) }
+  it { should belong_to(:license) }
+end
