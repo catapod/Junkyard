25ffd16aa993dd43081ec21a60d81dfcf20ec6c4
Дмитрий Басавин <basavind@gmail.com>
2017-04-09 11:35:40 +0300

    ✅  add validation tests for comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  # Validations
+  it { should validate_presence_of(:chunk) }
+  it { should validate_presence_of(:commentator) }
+  it { should validate_presence_of(:body) }
+
+  # Associations
+  it { should belong_to(:chunk) }
+  it { should belong_to(:commentator) }
+end
