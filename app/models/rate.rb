c46e5f90b1da0396181b2189c7254113e4ea5263
Дмитрий Басавин <basavind@gmail.com>
2017-04-09 11:50:55 +0300

    🚧  update rate model according to tests

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,8 @@
 class Rate < ApplicationRecord
-  belongs_to :user
+  belongs_to :rater, class_name: 'User'
   belongs_to :translation
+
+  validates :rater, presence: true
+  validates :translation, presence: true
+  validates :value, presence: true
 end
