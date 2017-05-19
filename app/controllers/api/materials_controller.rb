01dc1e99837f3999b265470afba89ae15a01072b
Michael Pet <mixty01@gmail.com>
2017-05-20 02:41:55 +0600

    Remove api assets

--- app/controllers/api/materials_controller.rb
+++ app/controllers/api/materials_controller.rb
@@ -1,17 +1,33 @@
 class Api::MaterialsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  def index
+  end
+
   def create
-    
   end
 
   def update
-    
   end
 
   def destroy
-    
   end
 
   def show
-    
   end
+
+  private
+
+    def material_params
+      params.permit(
+        :original_link,
+        :caption_original,
+        :caption_translated,
+        :annotation_original,
+        :annotation_translated,
+        :original_language,
+        :translation_language,
+        :tags
+      )      
+    end
 end
