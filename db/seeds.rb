50c23df7449e513bfb3459c29e3f25b54b056f11
Michael Pet <mixty01@gmail.com>
2017-05-21 01:59:30 +0600

    Add comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ ChunkType.create(
   ]
 )
 
+#==== For test ====#
 User.create(
   [
     { nickname: 'catapod' },
@@ -34,5 +35,20 @@ User.create(
     { nickname: 'mixty' },
     { nickname: 'zverart' },
     { nickname: 'garold' }
+  ]
+)
+
+Rightholder.create(
+  [
+    { caption_original: 'Massachusetts Institute of Technology', short_original: 'MIT' },
+    { caption_original: 'University of Oxford', short_original: 'Oxford' }
+  ]
+)
+
+License.create(
+  [
+    { name: 'Apache License 2.0', content: 'Apache License Version 2.0, January 2004 http://www.apache.org/licenses/ TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION 1. Definitions. "License" shall mean the terms and conditions for use, reproduction, and distribution as defined by Sections 1 through 9 of this document. "Licensor" shall mean the copyright owner or entity authorized by the copyright owner that is granting the License.' },
+    { name: 'BSD 3-Clause "New" or "Revised" license', content: 'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:' },
+    { name: 'MIT license', content: 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files...' }
   ]
 )
