0edc2fe376203dda0b8f508447ed04dd4621d607
Michael Pet <mixty01@gmail.com>
2017-05-21 03:08:31 +0600

    Add belongs_to methods to material serializer

--- app/serializers/material_serializer.rb
+++ app/serializers/material_serializer.rb
@@ -1,19 +1,19 @@
 class MaterialSerializer < ActiveModel::Serializer
   attributes(
-    :id,
-    :rightholder_id,
-    :owner_id,
     :original_link,
     :caption_original,
     :caption_translated,
     :annotation_original,
     :annotation_translated,
-    :state_id,
-    :license_id,
     :original_language,
     :translation_language,
     :tags
   )
 
-  has_many :chunks
+  belongs_to :rightholder
+  belongs_to :owner
+  belongs_to :state
+  belongs_to :license
+
+  # has_many :chunks
 end
