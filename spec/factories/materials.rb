ee6dad08f040dcc2372924899d95b9ab6b4789d0
Michael Pet <mixty01@gmail.com>
2017-05-23 00:52:29 +0600

    Create materials factory

---
+++ spec/factories/materials.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :material do
+    rightholder_id 1
+    owner_id 2
+    original_link { Faker::StarWars.character }
+    caption_original { Faker::StarWars.character }
+    caption_translated { Faker::StarWars.character }
+    annotation_original { Faker::StarWars.quote }
+    annotation_translated { Faker::StarWars.quote }
+    state_id 1
+    license_id 2
+    original_language { Faker::Hipster.word }
+    translation_language { Faker::Hipster.word }
+  end
+end
