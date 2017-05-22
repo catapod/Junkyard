2990fda163747fd4f1f47182d0e0eb1fe3d59751
Michael Pet <mixty01@gmail.com>
2017-05-22 20:19:49 +0600

    Generate translations controller and create CRUD methods

---
+++ app/controllers/api/translations_controller.rb
@@ -0,0 +1,41 @@
+class Api::TranslationsController < Api::ApiController
+  before_action :set_translation, only: [:show, :update, :destroy]
+  
+  def index
+    @translations = Translation.all
+    json_response(@translations)
+  end
+
+  def create
+    @translation = Translation.create!(translation_params)
+    json_response(@translation, :created)
+  end
+
+  def show
+    json_response(@translation)
+  end
+
+  def update
+    @translation.update(translation_params)
+    json_response(@translation)
+  end
+
+  def destroy
+    @translation.destroy
+    head :no_content
+  end
+
+  private
+
+    def set_translation
+      @translation = Translation.find(params[:id])
+    end
+
+    def translation_params
+      params.permit(
+        :chunk_id,
+        :translator_id,
+        :body
+      )      
+    end
+end
