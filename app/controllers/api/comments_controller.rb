77430c4a9b1a915fd3f1b37e867b77e7c008a67f
Michael Pet <mixty01@gmail.com>
2017-05-22 20:06:44 +0600

    Generate comments controller and create CRUD methods

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,41 @@
+class Api::CommentsController < Api::ApiController
+  before_action :set_comment, only: [:show, :update, :destroy]
+  
+  def index
+    @comments = Comment.all
+    json_response(@comments)
+  end
+
+  def create
+    @comment = Comment.create!(comment_params)
+    json_response(@comment, :created)
+  end
+
+  def show
+    json_response(@comment)
+  end
+
+  def update
+    @comment.update(comment_params)
+    json_response(@comment)
+  end
+
+  def destroy
+    @comment.destroy
+    head :no_content
+  end
+
+  private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.permit(
+        :chunk_id,
+        :commentator,
+        :body
+      )      
+    end
+end
