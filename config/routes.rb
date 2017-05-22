e98e053173be83ce3c34717a05f9af628e89f925
Michael Pet <mixty01@gmail.com>
2017-05-22 20:10:57 +0600

    Add route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :materials do
-      resources :chunks
+      resources :chunks do
+        resources :comments
+      end
     end
   end
 end
