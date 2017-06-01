9ea0194a03535fd91412db5bc4206dff9bd6c807
Michael Pet <mixty01@gmail.com>
2017-06-01 15:48:19 +0600

    Fix some fields

--- spec/factories/materials.rb
+++ spec/factories/materials.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :material do
-    rightholder_id 1
-    owner_id 2
+    rightholder
+    association :owner, factory: :user
     original_link { Faker::StarWars.character }
     caption_original { Faker::StarWars.character }
     caption_translated { Faker::StarWars.character }
     annotation_original { Faker::StarWars.quote }
     annotation_translated { Faker::StarWars.quote }
     state_id 1
-    license_id 2
+    license
     original_language { Faker::Hipster.word }
     translation_language { Faker::Hipster.word }
     tags { [1, 3, 2] }
@@ -17,8 +17,8 @@ FactoryGirl.define do
         chunks_count 5
       end
 
-      after(:create) do |material, evaluator|
-        create_list(:chunk, evaluator.chunks_count, material: material)
+      after :create do |material, evaluator|
+        create_list(:chunk_with_comments, evaluator.chunks_count, material: material)
       end
     end
   end
