7842614f581f42123316372f36a919ae8bdf23ce
Michael Pet <mixty01@gmail.com>
2017-06-01 15:47:34 +0600

    Add comments factory into chunks factory

--- spec/factories/chunks.rb
+++ spec/factories/chunks.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
   factory :chunk do
-    material_id 1
     material_position { Faker::Number.between(1, 5) }
     chunk_type_id 2
     translatable true
     body { Faker::ChuckNorris.fact }
+    factory :chunk_with_comments do
+      transient do
+        comments_count 5
+      end
+
+      after :create do |chunk, evaluator|
+        create_list(:comment, evaluator.comments_count, chunk: chunk)
+      end
+    end
   end
 end
