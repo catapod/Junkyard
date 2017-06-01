ec28caff5f154b9a76eced9535c4c74e029049f1
Michael Pet <mixty01@gmail.com>
2017-06-01 16:08:16 +0600

    Create translations list in chunks factory

--- spec/factories/chunks.rb
+++ spec/factories/chunks.rb
@@ -4,13 +4,14 @@ FactoryGirl.define do
     chunk_type_id 2
     translatable true
     body { Faker::ChuckNorris.fact }
-    factory :chunk_with_comments do
+    factory :chunk_with_comments_and_translations do
       transient do
         comments_count 5
       end
 
       after :create do |chunk, evaluator|
         create_list(:comment, evaluator.comments_count, chunk: chunk)
+        create_list(:translation, evaluator.comments_count, chunk: chunk)
       end
     end
   end
