22e09a0256d7983a8f9ed6820ae8a108c099b37b
Дмитрий Басавин <basavind@gmail.com>
2017-06-09 22:48:20 +0300

    ✅  simplify materials controller spec

--- spec/controllers/api/materials_controller_spec.rb
+++ spec/controllers/api/materials_controller_spec.rb
@@ -1,45 +1,51 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Api::MaterialsController, type: :controller do
-  before(:all) do
-    create_list(:user, 5)
-    create_list(:rightholder, 5)
-    create_list(:license, 5)
-    create_list(:material_tag, 5)
-  end
-  
-  let!(:materials) { create_list(:material, 10) }
-  let(:material_id) { materials.first.id }
-
   describe 'GET api/materials' do
     before { get :index }
-
-    it 'returns materials' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
-    end
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
+
+    context 'when there are no materials' do
+      it 'returns empty response' do
+        expect(json).to be_empty
+      end
+    end
+
+    context 'when there are materials' do
+      it 'returns them' do
+        create_list(:material, 5)
+
+        # call get once more to get materials after initialization
+        get :index
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(5)
+      end
+    end
   end
 
   describe 'GET api/materials/:id' do
-    before { get :show, params: { id: material_id } }
+    context 'when the material exists' do
+      let(:material) { create(:material) }
 
-    context 'when the record exists' do
-      it 'returns the material' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(material_id)
-      end
+      before { get :show, params: { id: material.id } }
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
+
+      it 'returns the material' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(material.id)
+      end
     end
 
-    context 'when the record does not exist' do
-      let(:material_id) { 200 }
+    context 'when the material does not exist' do
+      before { get :show, params: { id: 666 } }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -52,19 +58,24 @@ RSpec.describe Api::MaterialsController, type: :controller do
   end
 
   describe 'POST api/materials' do
+    let(:owner) { create(:user) }
+    let(:rightholder) { create(:rightholder) }
+    let(:state) { create(:state) }
+    let(:license) { create(:license) }
+    let(:material_tags) { create_list(:material_tag, 2) }
+
     let :valid_attributes do
       {
-        id: material_id,
-        rightholder_id: 1,
-        owner_id: 2,
-        state_id: 1,
+        rightholder_id: rightholder.id,
+        owner_id: owner.id,
+        state_id: state.id,
         caption_original: 'Objects in Python',
         caption_translated: 'Объекты в Python',
         annotation_original: 'This lecture introduces Python',
         annotation_translated: 'В данной лекции вводятся понятия языка Python',
-        license_id: 1,
-        "original_language": "en",
-        "translation_language": "ru",
+        license_id: license.id,
+        "original_language": 'en',
+        "translation_language": 'ru',
         tags: [
           {
             name: 'course',
@@ -77,13 +88,13 @@ RSpec.describe Api::MaterialsController, type: :controller do
             body: 'Electrical Engineering and Computer Science'
           },
           {
-            id: 2,
+            id: material_tags[0].id,
             name: 'course2',
             display_name: 'курс2',
             body: 'Electrical Engineering and Computer Science'
           },
           {
-            id: 1
+            id: material_tags[1].id
           }
         ]
       }
@@ -92,12 +103,12 @@ RSpec.describe Api::MaterialsController, type: :controller do
     context 'when the request is valid' do
       before { post :create, params: valid_attributes }
 
-      it 'creates a material' do
-        expect(json['caption_original']).to eq('Objects in Python')
-      end
-
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
+      end
+
+      it 'creates a material' do
+        expect(json['caption_original']).to eq('Objects in Python')
       end
     end
 
@@ -127,17 +138,27 @@ RSpec.describe Api::MaterialsController, type: :controller do
   end
 
   describe 'PUT api/materials/:id' do
-    let(:valid_attributes) { { id: material_id, caption_original: 'Updated caption_original' } }
+    let(:material) { create(:material) }
+    let(:valid_attributes) { { caption_original: 'Updated caption_original' } }
 
-    context 'when the record exists' do
-      before { put :update, params: valid_attributes }
-
-      it 'updated the record' do
-        expect(json).not_to be_empty
-      end
+    context 'when the material exists' do
+      before { put :update, params: { **valid_attributes, id: material.id } }
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
+      end
+
+      it 'updates the record' do
+        expect(json).not_to be_empty
+        expect(json['caption_original']).to eq('Updated caption_original')
+      end
+    end
+
+    context 'when the material exists' do
+      before { put :update, params: { **valid_attributes, id: 666 } }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
       end
     end
   end
@@ -145,8 +166,21 @@ RSpec.describe Api::MaterialsController, type: :controller do
   describe 'DELETE api/materials/:id' do
     before { delete :destroy, params: { id: material_id } }
 
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204)
+    context 'when the material exists' do
+      let(:material) { create(:material) }
+      let(:material_id) { material.id }
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+
+    context 'when the material does not exists' do
+      let(:material_id) { 999 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
