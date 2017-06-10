d9616b669f687ed7144e102fc6a352620cc1d63d
Дмитрий Басавин <basavind@gmail.com>
2017-06-10 10:06:25 +0300

    🚧  add license seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+License.create(
+  [
+    { name: 'Apache License 2.0', content: 'Apache License Version 2.0, January 2004 http://www.apache.org/licenses/ TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION 1. Definitions. "License" shall mean the terms and conditions for use, reproduction, and distribution as defined by Sections 1 through 9 of this document. "Licensor" shall mean the copyright owner or entity authorized by the copyright owner that is granting the License.' },
+    { name: 'BSD 3-Clause "New" or "Revised" license', content: 'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:' },
+    { name: 'MIT license', content: 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files...' }
+  ]
+)
+
 State.create(
   [
     { name: 'import', display_name: 'Импорт' },
