9ae01d8f2e36a75a74df5fd9a3aaefecf616886c
Дмитрий Басавин <basavind@gmail.com>
2017-07-02 13:37:44 +0300

    ✅  (api) move endpoint validation schemas into spec/api_schemas

--- spec/support/api_schema_matcher.rb
+++ spec/support/api_schema_matcher.rb
@@ -3,7 +3,7 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :match_response_schema do |schema|
   match do |response|
-    schema_directory = "#{Dir.pwd}/spec/support/api/schemas"
+    schema_directory = "#{Dir.pwd}/spec/api_schemas"
     schema_path = "#{schema_directory}/#{schema}.json"
 
     JSON::Validator.validate!(schema_path, response.body, strict: true)
