025ce8f2962210c0271bc93a98fe5f4487c195dc
Дмитрий Басавин <basavind@gmail.com>
2017-07-24 19:01:14 +0300

    🐛  (domain) fix model's nullable fields

--- db/migrate/20170406110610_create_translations.rb
+++ db/migrate/20170406110610_create_translations.rb
@@ -1,8 +1,8 @@
 class CreateTranslations < ActiveRecord::Migration[5.0]
   def change
     create_table :translations do |t|
-      t.belongs_to :chunk, index: true
-      t.references :translator, index: true, foreign_key: { to_table: :users }
+      t.belongs_to :chunk, index: true, null: false
+      t.references :translator, index: true, null: false, foreign_key: { to_table: :users }
       t.text :body, null: false
       t.timestamps
     end
