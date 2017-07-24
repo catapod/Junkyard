6e103d0f1620a0b8eb290e46d47b41fe29800b0f
Дмитрий Басавин <basavind@gmail.com>
2017-07-24 18:54:25 +0300

    🚧  (api) add check rate object existence before creating

--- app/controllers/api/rates_controller.rb
+++ app/controllers/api/rates_controller.rb
@@ -2,13 +2,27 @@
 module Api
   # Translation rates controller
   class RatesController < Api::ApiController
-    def rate
-      @rate = Rate.find_or_create_by!(rate_params)
+    before_action :set_rate
 
-      json_response(@rate)
+    def rate
+      if @rate.nil?
+        @rate = Rate.create!(rate_params)
+      else
+        @rate.value = params[:value]
+        @rate.save!
+      end
+
+      json_response @rate
     end
 
     private
+
+    def set_rate
+      @rate = Rate.find_by params.permit(
+        :rater_id,
+        :translation_id
+      ).to_h
+    end
 
     def rate_params
       params.permit(
--- spec/controllers/api/rates_controller_spec.rb
+++ spec/controllers/api/rates_controller_spec.rb
@@ -15,6 +15,32 @@ RSpec.describe Api::RatesController, type: :controller do
   end
 
   describe 'PUT api/materials/:material_id/chunks/:chunk_id/translations/:translation_id/rate' do
+    context 'when rater already has rate the translation' do
+      before { put :rate, params: { **route_params, value: 1, rater_id: rater.id } }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'updats existed rate with different value' do
+        prev_rate = json
+
+        put :rate, params: { **route_params, value: -1, rater_id: rater.id }
+
+        expect(json['id']).to eq(prev_rate['id'])
+        expect(json['updated_at']).to be > prev_rate['updated_at']
+        expect(json.except('updated_at', 'value')).to eq(prev_rate.except('updated_at', 'value'))
+      end
+
+      it 'does not updates existed rate with same value' do
+        prev_rate = json
+
+        put :rate, params: { **route_params, value: 1, rater_id: rater.id }
+
+        expect(json).to eq(prev_rate)
+      end
+    end
+
     context 'when rater rates up' do
       before { put :rate, params: { **route_params, value: 1, rater_id: rater.id } }
 
